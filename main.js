5c3c02db820e21dd04b4800593c8c07a7d869a57
Sam Wing <sampwing@gmail.com>
2016-09-29 18:45:15 -0700

    Some basic working components CommentBox CommentList CommentForm

--- main.js
+++ main.js
@@ -2,16 +2,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// React.createClass
-var CommentBox = React.createClass({displayName: 'CommentBox',
+var CommentBox = React.createClass({
   render: function() {
     return (
-      React.createElement('div', {className: 'commentBox'},
-        "Hello, i am comment box!."
-      )
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
     );
   }
-})
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello world. I am a comment List!
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello world, I am a commentForm!
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
   React.createElement(CommentBox, null),
