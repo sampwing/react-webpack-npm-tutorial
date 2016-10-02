ae33f5d3aacbc5a301c3a15edf45e59577e5a2f2
Sam Wing <sampwing@gmail.com>
2016-10-02 13:25:22 -0700

    Have a couple routes working to play around with

--- src/main.js
+++ src/main.js
@@ -1,14 +1,54 @@
 // main.js
 import React from "react";
 import ReactDOM from "react-dom";
-import $ from "jquery";
+import { Router, Route, Link, browserHistory } from "react-router";
 
 import CommentBox from "./components/CommentBox";
 
 import config from "./config";
 const APIUrl = config[process.env.NODE_ENV].api;
 
-ReactDOM.render(
-  <CommentBox url={APIUrl + "/comments"} pollInterval={10000} />,
+export class Home extends React.Component {
+  render() {
+    return(
+    <div>
+      <h1>Home</h1>
+      <h1><Link to={`/about`}>About</Link></h1>
+      <h1><Link to={`/comments`}>Comments</Link></h1>
+    </div>)
+  }
+}
+
+export class About extends React.Component {
+  render() {
+    return(
+    <div>
+      <h1><Link to={`/`}>Home</Link></h1>
+      <h1>About</h1>
+      <h1><Link to={`/comments`}>Comments</Link></h1>
+    </div>)
+  }
+}
+
+export class Comments extends React.Component {
+  render() {
+    return(
+    <div>
+      <h1><Link to={`/`}>Home</Link></h1>
+      <h1><Link to={`/about`}>About</Link></h1>
+      <h1>Comments</h1>
+      <div>
+        <CommentBox url={APIUrl + "/comments"} pollInterval={10000} />
+      </div>
+    </div>)
+  }
+}
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} / >
+    <Route path="/comments" component={Comments} />
+  </Router>),
   document.getElementById('content')
 );
