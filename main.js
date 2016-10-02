c379045460828256893b880e57c6c961bdf245e7
Sam Wing <sampwing@gmail.com>
2016-10-01 18:22:46 -0700

    Everything seems to be working now

--- main.js
+++ main.js
@@ -27,13 +27,16 @@ var CommentBox = React.createClass({
     comment.id = Date.now();
     var newComments = comments.concat([comment]);
     this.setState({data: newComments});
+    console.log(this.state.data);
     $.ajax({
       url: this.props.url,
       dataType: 'json',
       type: 'POST',
       data: comment,
       success: function(data) {
-        this.setState({data: data});
+        console.log(data);
+
+        //this.setState({data: data});
       }.bind(this),
       error: function(xhr, status, err) {
         this.setState({data: comments});
